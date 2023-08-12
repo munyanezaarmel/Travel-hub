b607d9098188931e65ecf11ec9708fd16b485844
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2023-08-12 09:09:01 +0200

    feat:added destination section

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,13 +3,15 @@ import Header from "./Header";
 import Hero from "./Hero";
 import Form from "./Form";
 import Recent from "./Recent";
+import TrendingDestination from "./trendingDestination";
 export default function Landing() {
   return (
     <div>
       <Header />
       <Hero />
       <Form />
-      <Recent/>
+      <Recent />
+      <TrendingDestination/>
     </div>
   );
 }
---
+++ src/components/trendingDestination.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import desk from '../assets/desk.png'
+
+export default function TrendingDestination() {
+  return (
+    <div>
+      <h1 className="ml-20 font-bold font-inter text-[24px]  ">
+        Trending destinations
+      </h1>
+      <p className="ml-20 text-[#333] text-[16px] font-inter">
+        travellers searching for Rwanda also booked this
+      </p>
+      <div className="grid grid-cols-3 p-8 m-8">
+        <img src={desk} alt="trending image" className="rounded-lg " />
+        <img src={desk} alt="trending image" className="rounded-lg ml-4 mr-4" />
+        <img src={desk} alt="trending image" className="rounded-lg ml-8 " />
+        <img src={desk} alt="trending image" className="rounded-lg mt-4 " />
+        <img src={desk} alt="trending image" className="rounded-lg ml-4 mt-4" />
+      </div>
+    </div>
+  );
+}
